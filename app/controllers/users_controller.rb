d8bfcf9ebf4f0e1769b763f9feb140119a0292fa
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-03-19 17:30:14 -0400

    add user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save()
+            flash[:success] = 'you are now registered!'
+            redirect_to home_path
+        else
+            render 'new'
+        end 
+    end
+
+
+    private
+
+    def user_params
+        params.require('user').permit(:username)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'events#index'
+  root 'events#index' , as: 'home'
+
+  get '/signup' => 'users#new'  , as: 'signup'
+  post '/signup' => 'users#create'
 
 end
