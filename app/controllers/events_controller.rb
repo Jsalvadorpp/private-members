b5d34bd9ef2d92a36a08e4ca4f22f64c3d00cc81
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-03-20 15:45:44 -0400

    user can create new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,29 @@ class EventsController < ApplicationController
     def index
     end
 
+    def new
+        @user = current_user
+        @event = Event.new
+    end
+
+    def create
+        @user = current_user
+        @event = @user.events.new(event_params)
+
+        if @event.save
+            flash[:success] = 'Event created!'
+            redirect_to profile_path
+        else
+            render 'new'
+        end
+    end
+
+
+    private 
+
+    def event_params
+        params.require('event').permit(:title,:description,:date,:location)
+    end
+    
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :events,foreign_key: :creator_id
 
 
     def generate_token
