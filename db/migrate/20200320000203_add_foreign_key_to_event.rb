71c4f54955a3927443d3e5841592bfdd672f4d6d
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-03-19 23:54:52 -0400

    add event model

---
+++ db/migrate/20200320000203_add_foreign_key_to_event.rb
@@ -0,0 +1,7 @@
+class AddForeignKeyToEvent < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :events, :creator, references: :users
+    add_foreign_key :events, :users, column: :creator_id
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_224001) do
+ActiveRecord::Schema.define(version: 2020_03_20_000203) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "location"
+    t.datetime "date"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_03_19_224001) do
     t.string "remember_token"
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
