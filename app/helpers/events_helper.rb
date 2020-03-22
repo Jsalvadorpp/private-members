38497f027a444c190abe7a3d78bc19d7796c1e1b
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-03-22 19:25:42 -0400

    user cant attend to expired events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -5,4 +5,10 @@ module EventsHelper
         event = Event.find_by(id: params['id'])
         return !event.invitations.where(attendee: current_user).exists?
     end
+
+    def event_not_expired?
+        event = Event.find_by(id: params['id'])
+        is_expired = (event.date <= DateTime.now)? true : false
+        return !is_expired
+    end
 end
